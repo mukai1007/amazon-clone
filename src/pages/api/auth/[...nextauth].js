c2c9007f07df61695933a36af8d9d8545ce34cbe
user <user@users-MacBook-Air-2.local>
2023-05-02 01:38:24 +0600

    change nextAuth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,10 +9,6 @@ export default NextAuth ({
         clientId: '929648318218-ke31o4c3cfh66uukgujs04tbn2ubfc3o.apps.googleusercontent.com',
         clientSecret: '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'
     }),
-    FaceBookProvider({
-      clientId: 'msklmcsldmcio34092dkwskcml4034=fe-rf3mdsnsckmdc',
-      clientSecret: 'sdcmkrfmo34ij340f9jdsk '
-  }),
     // ...add more providers here
   ],
 })
